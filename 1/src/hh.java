c74e268396d3d368a1c19f356fb58818adc178b3
Kashirskij Artyom <90215208+kshrskart@users.noreply.github.com>
2022-02-22 19:23:03 +0300

    1taskJava
    hh - №1
    hh - №1
    hh - №1

---
+++ 1/src/hh.java
@@ -0,0 +1,11 @@
+
+public class hh {
+	public static void main(String[] args) {
+		if (args.length != 0) {
+			System.out.println("\tHello, "+ args[0] + "!");
+		}
+		else {
+			System.out.println("\tHello, World!");
+		}
+	}
+}
