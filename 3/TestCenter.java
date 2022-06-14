19cc9fb9f9c9a82014ae40d3b6592313d53d41b8
Kashirskii Artyom <90215208+kshrskart@users.noreply.github.com>
2022-06-14 14:37:36 +0300

    3

---
+++ 3/TestCenter.java
@@ -0,0 +1,8 @@
+package com.makotogroup.intro;
+
+public class TestCenter {
+    public static void main(String[] args) {
+        ServiceCenter servicecenter = new ServiceCenter("1st New-Moscow AutoCenter","Workerss, 50","Hondss Civic III","2 tires","12/05/2000","15/05/2000","Completed","'Ternenko I.A'",1235);
+        System.out.println(servicecenter);
+    }
+}
